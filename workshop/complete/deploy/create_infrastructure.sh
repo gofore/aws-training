9b9f44c9810ea2b29d9d56997bba802f180097f4
Ville Seppanen <ville.seppanen@gofore.com>
2014-11-12 20:12:04 +0200

    Added helper script for launching stack

--- workshop/complete/deploy/create_infrastructure.sh
+++ workshop/complete/deploy/create_infrastructure.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-ansible-playbook -v -i ansible-inventory-localhost create_infrastructure.yml
+HELP="Usage: $0 YOUR_USER_NAME YOUR_EMAIL_ADDRESS"
+
+if [ $# -ne 2 ]
+  then
+    echo "Error: wrong number of arguments\n$HELP"
+    exit 1
+fi
+
+USER_NAME="$1"
+EMAIL="$2"
+
+ansible-playbook -e "{\"user_name\":\"$USER_NAME\",\"user_email\":\"$EMAIL\"}" -v -i ansible-inventory-localhost create_infrastructure.yml
