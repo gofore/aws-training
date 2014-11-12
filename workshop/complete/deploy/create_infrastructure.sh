859dec29dcea3ada9ffe4859a27a84ba3aa43ed2
Ville Seppanen <ville.seppanen@gofore.com>
2014-11-12 23:47:46 +0200

    Parametrize key name

--- workshop/complete/deploy/create_infrastructure.sh
+++ workshop/complete/deploy/create_infrastructure.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-HELP="Usage: $0 YOUR_USER_NAME YOUR_EMAIL_ADDRESS"
+HELP="Usage: $0 YOUR_USER_NAME YOUR_EMAIL_ADDRESS KEY_NAME"
 
-if [ $# -ne 2 ]
+if [ $# -ne 3 ]
   then
     echo "Error: wrong number of arguments\n$HELP"
     exit 1
@@ -10,5 +10,6 @@ fi
 
 USER_NAME="$1"
 EMAIL="$2"
+KEY_NAME="$3"
 
-ansible-playbook -e "{\"user_name\":\"$USER_NAME\",\"user_email\":\"$EMAIL\"}" -i ansible-inventory-localhost create_infrastructure.yml
+ansible-playbook -e "{\"user_name\":\"$USER_NAME\",\"user_email\":\"$EMAIL\",\"key_name\":\"$KEY_NAME\"}" -i ansible-inventory-localhost create_infrastructure.yml
