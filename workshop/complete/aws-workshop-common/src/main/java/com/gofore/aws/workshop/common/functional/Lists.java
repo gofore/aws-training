8f4a06e49fc0fdf625488ff4f75e72cd274881c8
trautonen <trautonen@github.com>
2014-11-14 01:27:23 +0200

    Functional cleanup.

--- workshop/complete/aws-workshop-common/src/main/java/com/gofore/aws/workshop/common/functional/Lists.java
+++ workshop/complete/aws-workshop-common/src/main/java/com/gofore/aws/workshop/common/functional/Lists.java
@@ -1,13 +1,23 @@
 package com.gofore.aws.workshop.common.functional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Lists {
     
     public static <F, T> List<T> map(List<F> list, Function<F, T> function) {
         return list.stream().map(function).collect(Collectors.toList());
+    }
+    
+    public static <T> Optional<T> findFirst(List<T> list, Predicate<T> predicate) {
+        return list.stream().filter(predicate).findFirst();
+    }
+    
+    public static <T> Function<List<T>, Optional<T>> findFirst(Predicate<T> predicate) {
+        return l -> findFirst(l, predicate);
     }
     
     private Lists() {
--- workshop/complete/aws-workshop-common/src/main/java/com/gofore/aws/workshop/common/simpledb/DomainLookup.java
+++ workshop/complete/aws-workshop-common/src/main/java/com/gofore/aws/workshop/common/simpledb/DomainLookup.java
@@ -1,11 +1,10 @@
 package com.gofore.aws.workshop.common.simpledb;
 
-import java.util.Collection;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 import com.amazonaws.services.simpledb.model.ListDomainsRequest;
 import com.amazonaws.services.simpledb.model.ListDomainsResult;
+import com.gofore.aws.workshop.common.functional.Lists;
 import com.gofore.aws.workshop.common.properties.ApplicationProperties;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -25,9 +24,7 @@ public class DomainLookup {
     public String getImagesDomain() {
         return simpleDBClient.listDomains(new ListDomainsRequest())
                 .thenApply(ListDomainsResult::getDomainNames)
-                .thenApply(Collection::stream)
-                .thenApply(s -> s.filter(d -> d.startsWith(imagesDomainPrefix)))
-                .thenApply(Stream::findFirst)
+                .thenApply(Lists.findFirst(d -> d.startsWith(imagesDomainPrefix)))
                 .thenApply(Optional::get)
                 .join();
     }
