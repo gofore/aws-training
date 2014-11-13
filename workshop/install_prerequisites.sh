989b8c47b50474f8a3aeb4001c30301270c5a75b
Ville Seppanen <ville.seppanen@gofore.com>
2014-11-13 15:35:16 +0200

    Fix script rename

--- workshop/install_prerequisites.sh
+++ workshop/install_prerequisites.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Install prerequisite packages
 sudo add-apt-repository -y ppa:webupd8team/java
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 
@@ -10,3 +11,6 @@ sudo pip install boto ansible
 wget http://ppa.launchpad.net/natecarlson/maven3/ubuntu/pool/main/m/maven3/maven3_3.2.1-0~ppa1_all.deb
 sudo dpkg -i maven3_3.2.1-0~ppa1_all.deb
 sudo ln -s /usr/bin/mvn3 /usr/bin/mvn
+
+# Export AWS credentials from CSV file
+# echo "\n. /vagrant/workshop/export_env_variables.sh\n" >> ~/.bashrc
