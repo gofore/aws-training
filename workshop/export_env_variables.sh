4d9fe3b65dbcaaf4b50a3ec18140aae66bf04540
Ville Seppanen <ville.seppanen@gofore.com>
2014-11-17 07:26:10 +0200

    Fixed bug that closed terminal if file was not found

--- workshop/export_env_variables.sh
+++ workshop/export_env_variables.sh
@@ -11,25 +11,25 @@
 FILE="credentials.csv"
 
 if [ -n "$1" ]
-  then
-    FILE=$1
-fi
-
-if [ ! -f "$FILE" ]
 then
-    echo "File $FILE does not exists"
-    exit 1
+  FILE=$1
 fi
 
-CSV_USER=`tail -n +2 $FILE|cut -d , -f 1| tr -d '"'`
-CSV_ACCESS_KEY=`tail -n +2 $FILE|cut -d , -f 2`
-CSV_SECRET_KEY=`tail -n +2 $FILE|cut -d , -f 3`
+if [ -f "$FILE" ]
+then
+  CSV_USER=`tail -n +2 $FILE|cut -d , -f 1| tr -d '"'`
+  CSV_ACCESS_KEY=`tail -n +2 $FILE|cut -d , -f 2`
+  CSV_SECRET_KEY=`tail -n +2 $FILE|cut -d , -f 3`
 
-export AWS_WORKSHOP_USER=$CSV_USER
-export AWS_ACCESS_KEY=$CSV_ACCESS_KEY
-export AWS_ACCESS_KEY_ID=$CSV_ACCESS_KEY
-export AWS_SECRET_KEY=$CSV_SECRET_KEY
-export AWS_SECRET_ACCESS_KEY=$CSV_SECRET_KEY
+  export AWS_WORKSHOP_USER=$CSV_USER
+  export AWS_ACCESS_KEY=$CSV_ACCESS_KEY
+  export AWS_ACCESS_KEY_ID=$CSV_ACCESS_KEY
+  export AWS_SECRET_KEY=$CSV_SECRET_KEY
+  export AWS_SECRET_ACCESS_KEY=$CSV_SECRET_KEY
 
-echo "The following AWS environment variables are now set:"
-printenv|grep AWS_
+  echo "The following AWS environment variables are now set:"
+  printenv|grep AWS_
+
+else
+	echo "File $FILE does not exists"
+fi
