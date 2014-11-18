64f33b71f466e72b2eeb9c9d623aeba74872f32a
Ville Seppanen <ville.seppanen@gofore.com>
2014-11-18 16:12:26 +0200

    Added documentation to tools

--- workshop/initial/deploy/hot_update_servers.sh
+++ workshop/initial/deploy/hot_update_servers.sh
@@ -6,7 +6,7 @@
 # EC2 tags.
 #
 # Example usage:
-# ./hot_update_servers.sh myusername ../mykeyfile.pem fetcher ubuntu
+# ./hot_update_servers.sh myusername path/to/mykeyfile.pem fetcher ubuntu
 
 
 HELP="Usage: $0 YOUR_USER_NAME PATH_TO_SSH_KEYFILE APP_TO_UPDATE SSH_USER_NAME\nExample: ./hot_update_servers.sh myusername ../mykeyfile.pem fetcher ubuntu\nAPP_TO_UPDATE is one of ui, loader, fetcher."
@@ -22,15 +22,21 @@ KEY_PATH="$2"
 APP="$3"
 SSH_USER="$4"
 
-PREFIX="aws-workshop-"
+STACK_PREFIX="aws-workshop"
 PLAYBOOK="hot_update_servers.yml"
 
-read -r -p "Warning: Are you sure you want to update '$PREFIX$USER_NAME-$APP' with the latest jar from S3? [y/N] " response
+read -r -p "Warning: Are you sure you want to update '$STACK_PREFIX-$USER_NAME-$APP' with the latest jar from S3? [y/N] " response
 response=${response,,} # to lower-case
 
 if [[ $response =~ ^(yes|y)$ ]]; then
 
-  ansible-playbook -e "app_name=$PREFIX$APP user_name=$USER_NAME" --limit=tag_Name_$PREFIX$USER_NAME-$APP -v -u $SSH_USER --private-key=$KEY_PATH -i ansible-dynamic-inventory/ec2.py $PLAYBOOK
+  # -e passes variables to the ansible playbook
+  # -i forms an inventory list of all the EC2 instances
+  # --limit limits the target machines from the full inventory to only the ones with specific name tag
+  # -u is the user which ansible uses to ssh into the target instances
+  # --private-key is the ssh key to use on the target instances
+  # -v adds verbosity
+  ansible-playbook -e "app_name=$STACK_PREFIX-$APP user_name=$USER_NAME" --limit=tag_Name_$STACK_PREFIX-$USER_NAME-$APP -v -u $SSH_USER --private-key=$KEY_PATH -i ansible-dynamic-inventory/ec2.py $PLAYBOOK
 
 else
   echo "Aborting."
